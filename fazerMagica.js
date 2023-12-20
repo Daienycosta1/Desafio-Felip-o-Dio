decfa28fa5d7dcc2153e76eef1207326104547bf
Daienycosta1 <daienycosta1@gmail.com>
2023-12-20 18:19:32 -0400

    Calculadora de Rankeada

---
+++ fazerMagica.js
@@ -0,0 +1,3 @@
+function fazerMagica() {
+    console.log("Abracadabra! COISAS Magicas acontecem");
+}
---
+++ função.js
@@ -0,0 +1,8 @@
+torrar("pão de forma")
+
+
+function torrar(pao , nome = "Cliente"){
+    console.log("torrada feita com " + pao)
+    console.log("ele e um pedido de " + nome)
+}
+//função de parametro geralmente e usanda quando queremos que uma mesma função tenha resultados diferentes
